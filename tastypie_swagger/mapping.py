c3873cebb91a806c5f36e93f801cc0bb58f23d4d
Rudy <rudy@yetitechnologies.com>
2012-12-02 15:56:54 -0800

    tastypie filtering parameters on resources

--- tastypie_swagger/mapping.py
+++ tastypie_swagger/mapping.py
@@ -6,6 +6,10 @@ from .utils import trailing_slash_or_none, urljoin_forced
 # Ignored POST fields
 IGNORED_FIELDS = ['id', ]
 
+# Enable all basic ORM filters but do not allow filtering across relationships.
+ALL = 1
+# Enable all ORM filters, including across relationships
+ALL_WITH_RELATIONS = 2
 
 class ResourceSwaggerMapping(object):
     """
@@ -52,6 +56,34 @@ class ResourceSwaggerMapping(object):
                 ))
         return parameters
 
+    def build_parameters_from_filters(self, prefix=""):
+        parameters = []
+        if 'filtering' in self.schema:
+            for name, field in self.schema['filtering'].items():
+                # Integer value means this points to a related model
+                if isinstance( field, int ):
+                    if field == ALL: #TODO: Show all possible ORM filters for this field
+                        pass
+                    elif field == ALL_WITH_RELATIONS: # Show all params from related model
+                        related_resource = self.resource.fields[name].get_related_resource(None)
+                        related_mapping = ResourceSwaggerMapping(related_resource)
+                        parameters.extend(related_mapping.build_parameters_from_filters(prefix="%s%s__" % (prefix, related_mapping.resource_name)))
+                elif isinstance( field, list ):
+                    # Skip if this is an incorrect filter
+                    if name not in self.schema['fields']: continue
+
+                    schema_field = self.schema['fields'][name]
+                    for query in field:
+                        parameters.append(self.build_parameter(
+                            paramType="query",
+                            name="%s%s__%s" % (prefix, name, query),
+                            dataType=schema_field['type'],
+                            required=schema_field['blank'],
+                            description=unicode(schema_field['help_text']),
+                        ))
+
+        return parameters
+
     def build_detail_operation(self, method='get'):
         operation = {
             'httpMethod': method.upper(),
@@ -64,10 +96,10 @@ class ResourceSwaggerMapping(object):
     def build_list_operation(self, method='get'):
         return {
             'httpMethod': method.upper(),
-            'parameters': [],
+            'parameters': self.build_parameters_from_filters(),
             'responseClass': 'List',
             'nickname': '%s-list' % self.resource_name,
-        }
+            }
 
     def build_detail_api(self):
         detail_api = {
