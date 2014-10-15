0575913e5b5f86e955e1b764c163a64039932d0f
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2014-10-15 18:09:21 -0300

    Add PATCH to supported detail and list methods

--- tastypie_swagger/mapping.py
+++ tastypie_swagger/mapping.py
@@ -37,6 +37,8 @@ class ResourceSwaggerMapping(object):
         'post-list': "Create a new %s",
         'put-detail': "Update an existing %s",
         'delete-detail': "Delete an existing %s",
+        'patch-detail': "Partialy update an existing %s",
+
     }
 
     def __init__(self, resource):
@@ -363,6 +365,11 @@ class ResourceSwaggerMapping(object):
             operation['parameters'].append(self.build_parameter_for_object(method='put'))
             detail_api['operations'].append(operation)
 
+        if 'patch' in self.schema['allowed_detail_http_methods']:
+            operation = self.build_detail_operation(method='patch')
+            operation['parameters'].append(self.build_parameter_for_object(method='patch'))
+            detail_api['operations'].append(operation)
+
         if 'delete' in self.schema['allowed_detail_http_methods']:
             detail_api['operations'].append(self.build_detail_operation(method='delete'))
 
@@ -380,6 +387,11 @@ class ResourceSwaggerMapping(object):
         if 'post' in self.schema['allowed_list_http_methods']:
             operation = self.build_list_operation(method='post')
             operation['parameters'].append(self.build_parameter_for_object(method='post'))
+            list_api['operations'].append(operation)
+
+        if 'patch' in self.schema['allowed_list_http_methods']:
+            operation = self.build_list_operation(method='patch')
+            operation['parameters'].append(self.build_parameter_for_object(method='patch'))
             list_api['operations'].append(operation)
 
         return list_api
@@ -426,8 +438,8 @@ class ResourceSwaggerMapping(object):
         for name, field in self.schema['fields'].items():
             if name in excludes:
                 continue
-            # Exclude fields from custom put / post object definition
-            if method in ['post','put']:
+            # Exclude fields from custom put / post / patch object definition
+            if method in ['post', 'put', 'patch']:
                 if name in self.WRITE_ACTION_IGNORED_FIELDS:
                     continue
                 if field.get('readonly'):
@@ -544,6 +556,15 @@ class ResourceSwaggerMapping(object):
                     resource_name='%s_put' % self.resource._meta.resource_name,
                     properties=self.build_properties_from_fields(method='put'),
                     id='%s_put' % self.resource_name
+                )
+            )
+
+        if 'patch' in self.resource._meta.detail_allowed_methods:
+            models.update(
+                self.build_model(
+                    resource_name='%s_patch' % self.resource._meta.resource_name,
+                    properties=self.build_properties_from_fields(method='patch'),
+                    id='%s_patch' % self.resource_name
                 )
             )
 
