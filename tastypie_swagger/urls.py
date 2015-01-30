0d9bd877b6b928e8d5ad2f1a4d13f9413345859b
Jim Lyndon <jimlyndon@gmail.com>
2015-01-29 20:44:36 -0800

    Update urls.py
    removed trailing slash to resolve tastypie endpoints correctly (not relying on append slash).
    See issue https://github.com/concentricsky/django-tastypie-swagger/issues/89

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -8,6 +8,6 @@ from .views import SwaggerView, ResourcesView, SchemaView
 urlpatterns = patterns('',
     url(r'^$', SwaggerView.as_view(), name='index'),
     url(r'^resources/$', ResourcesView.as_view(), name='resources'),
-    url(r'^schema/(?P<resource>\S+)/$', SchemaView.as_view()),
+    url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
     url(r'^schema/$', SchemaView.as_view(), name='schema'),
 )
