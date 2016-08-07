1cef8bbd87d8a293e9d5d5e2d0f313fbe78cb033
Aleksey Ropan <aropan@tut.by>
2016-08-07 19:38:28 +0300

    Fixed compatibility with Django 1.10

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,13 +1,23 @@
 try:
-	from django.conf.urls import patterns, include, url
+    from django.conf.urls import url
 except ImportError:
-	from django.conf.urls.defaults import patterns, include, url
+    from django.conf.urls.defaults import url
 
 from .views import SwaggerView, ResourcesView, SchemaView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', SwaggerView.as_view(), name='index'),
     url(r'^resources/$', ResourcesView.as_view(), name='resources'),
     url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
-    url(r'^schema/$', SchemaView.as_view(), name='schema'),
-)
+    url(r'^schema/$', SchemaView.as_view(), name='schema')
+]
+
+from django import get_version
+from distutils.version import StrictVersion
+
+if StrictVersion(get_version()) < StrictVersion("1.10"):
+    try:
+        from django.conf.urls import patterns
+    except ImportError:
+        from django.conf.urls.defaults import patterns
+    urlpatterns = patterns('', *urlpatterns)
