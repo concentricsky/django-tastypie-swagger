2a2ae38071c53da841aa3a749f2bf936620e5237
Marcin Swiderski <marcinswiderski@opentopic.com>
2018-04-16 14:25:01 +0200

    Added django 1.10 support

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,11 +1,23 @@
 try:
-	from django.conf.urls import patterns, include, url
+    from django.conf.urls import url
 except ImportError:
-	from django.conf.urls.defaults import patterns, include, url
+    from django.conf.urls.defaults import url
+
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    try:
+        from django.conf.urls.defaults import patterns
+    except ImportError:
+
+        def patterns(name, *args):
+            return args
+
 
 from .views import SwaggerView, ResourcesView, SchemaView
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', SwaggerView.as_view(), name='index'),
     url(r'^resources/$', ResourcesView.as_view(), name='resources'),
     url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
