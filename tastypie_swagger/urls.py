33043e46dd3d8a00ba7407bc73f061c0312f31eb
Neelanshu <6545006+thefinickycoder@users.noreply.github.com>
2018-10-10 02:59:56 +0530

    compatability for django 1.1x.x

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,13 +1,19 @@
-try:
-	from django.conf.urls import patterns, include, url
-except ImportError:
-	from django.conf.urls.defaults import patterns, include, url
-
 from .views import SwaggerView, ResourcesView, SchemaView
+try:
+    from django.conf.urls import include, url
 
-urlpatterns = patterns('',
-    url(r'^$', SwaggerView.as_view(), name='index'),
-    url(r'^resources/$', ResourcesView.as_view(), name='resources'),
-    url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
-    url(r'^schema/$', SchemaView.as_view(), name='schema'),
-)
+    urlpatterns = [
+        url(r'^$', SwaggerView.as_view(), name='index'),
+        url(r'^resources/$', ResourcesView.as_view(), name='resources'),
+        url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
+        url(r'^schema/$', SchemaView.as_view(), name='schema'),
+    ]
+except ImportError:
+    from django.conf.urls.defaults import patterns, include, url
+
+    urlpatterns = patterns('',
+        url(r'^$', SwaggerView.as_view(), name='index'),
+        url(r'^resources/$', ResourcesView.as_view(), name='resources'),
+        url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
+        url(r'^schema/$', SchemaView.as_view(), name='schema'),
+    )
