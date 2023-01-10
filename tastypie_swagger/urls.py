d256a5b7d29e1c2c4535b56d81a1a75f4484d6f0
abarqueroc <90042773+abarqueroc@users.noreply.github.com>
2023-01-10 17:30:30 +0100

    Update urls.py

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,13 +1,13 @@
 try:
-	from django.conf.urls import patterns, include, url
-except ImportError:
+	from django.conf.urls import url, include
+except (ImportError, ModuleNotFoundError):
 	from django.conf.urls.defaults import patterns, include, url
 
 from .views import SwaggerView, ResourcesView, SchemaView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', SwaggerView.as_view(), name='index'),
     url(r'^resources/$', ResourcesView.as_view(), name='resources'),
-    url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
+    url(r'^schema/(?P<resource>\S+)/$', SchemaView.as_view()),
     url(r'^schema/$', SchemaView.as_view(), name='schema'),
-)
+]
